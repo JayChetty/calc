b09163b5071ea35df9b53c61903bba5aa70fc993
JayChetty <jaypaulchetty@gmail.com>
2015-07-24 10:23:40 +0100

    working nicely

--- client/main.js
+++ client/main.js
@@ -7,23 +7,35 @@ var Engine = Matter.Engine,
 window.onload = function(){
 	console.log('app started', Matter);
 
+  var engine = Engine.create(document.getElementById('main-view'));
+
+  var height = 800;
+  var width = 800;
+  var canvas = document.getElementsByTagName("canvas")[0];
+  canvas.height = height;
+  canvas.width = width;
+  console.log('canvas', canvas)
+
   var boxHeight = 100
-  var pieceWidth = 10
+  var pieceDiameter = 10
   var boxAngle = -Math.PI * 0.075
 
-  var ballFriction = 0.00000001;
+  var ballFriction = 0.00001;
   var ballRestitution = 0.0000;
   var ballDensity = 0.00000000001;
   var ballSlop = 0;
 
 
-  var engine = Engine.create(document.body);
+  
   var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true  });
-
 
 
    // add ground
   World.add(engine.world, [ground]);
+  var ballBoxStartX = 500;
+  var ballStartX = ballBoxStartX+ 10;
+  var bottomWidth = 100
+  var bottom = Bodies.rectangle(ballBoxStartX + bottomWidth/2, 120, 100, 1, { isStatic: true, angle: 0 });
 
   //add catch box
   World.add(engine.world, [
@@ -35,9 +47,31 @@ window.onload = function(){
     // wee adjuster
     // Bodies.rectangle(313, 512, 40, 20, { isStatic: true, angle: Math.PI * 0.5 }),
     //catch helper
-    Bodies.rectangle(465, 280, 100, 2, { isStatic: true, angle: Math.PI * 0.5 }),
+
+    
+    //Ball Boxes
+    Bodies.rectangle(ballBoxStartX, 70, 100, 1, { isStatic: true, angle: Math.PI * 0.5 }),
+    Bodies.rectangle(ballBoxStartX + 20, 70, 100, 1, { isStatic: true, angle: Math.PI * 0.5 }),
+    Bodies.rectangle(ballBoxStartX + 40, 70, 100, 1, { isStatic: true, angle: Math.PI * 0.5 }),
+    //bottom
+    bottom,
+    
+    Bodies.circle(ballStartX, 60, 9.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop }),
+    Bodies.circle(ballStartX, 40, 9.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop }),
+    Bodies.circle(ballStartX, 20, 9.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop }),
+    Bodies.circle(ballStartX, 0, 9.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop }),
+
+    Bodies.circle(ballStartX+20, 60, 9.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop }),
+    Bodies.circle(ballStartX+20, 40, 9.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop }),
+    Bodies.circle(ballStartX+20, 20, 9.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop }),
+    Bodies.circle(ballStartX+20, 0, 9.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop }),
+
     // feed
     Bodies.rectangle(640, 210, 300, 2, { isStatic: true, angle: boxAngle }),
+    Bodies.rectangle(ballBoxStartX + 75, 150, 150, 2, { isStatic: true, angle: Math.PI * 0.04 })
+    ,
+    Bodies.rectangle(465, 281, 100, 2, { isStatic: true, angle: Math.PI * 0.5 }),
+
 
     // Bodies.trapezoid(600, 100, 15, 15, -0.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop })
     // Bodies.rectangle(100, 300, 400, 20, { isStatic: true, angle: Math.PI * 0.5 })
@@ -46,33 +80,46 @@ window.onload = function(){
     // Bodies.rectangle(275, 479, 300, 1, { isStatic: true, angle: boxAngle }),
     // Bodies.rectangle(275, 457, 300, 1, { isStatic: true, angle: boxAngle }),
     // Bodies.rectangle(275, 436, 300, 1, { isStatic: true, angle: boxAngle }),
-    // Bodies.rectangle(275, 415, 300, 1, { isStatic: true, angle: boxAngle }),
 
   ]);
 
   //add circles
 
 
-  var dropBalls = function(){
-    var newCircles = []
+  // var dropBalls = function(){
+  //   var newCircles = []
     
-    for(var i=0; i<1; i++){
-      var circle = Bodies.circle(600 + i*5, 100, 9.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop });
-      // var circle = Bodies.trapezoid(600, 100, 15, 15, 0.9, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop })
-      // var circle = Bodies.trapezoid(600, 100, 15, 15, 0.9, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop })
-      newCircles.push(circle)
-    }
-    World.add(engine.world, newCircles)    
-  }
+  //   for(var i=0; i<1; i++){
+  //     var circle = Bodies.circle(600 + i*5, 100, 9.5, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop });
+  //     // var circle = Bodies.trapezoid(600, 100, 15, 15, 0.9, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop })
+  //     // var circle = Bodies.trapezoid(600, 100, 15, 15, 0.9, { friction: ballFriction, restitution: ballRestitution, density: ballDensity, slop:ballSlop })
+  //     newCircles.push(circle)
+  //   }
+  //   World.add(engine.world, newCircles)
+  // }
+
 
 
   // run the engine
   Engine.run(engine);
-  for(var i=0; i<60; i++){
-    setTimeout(dropBalls, i*500);
-  }
+
+  //test drop random balls
+  // for(var i=0; i<21; i++){
+  //   setTimeout(dropBalls, i*500);
+  // }
+
+  // setTimeout(function(){
+  //   World.add(engine.world,[Bodies.rectangle(480, 452, 18, 1, { isStatic: true, angle: boxAngle, restitution: ballRestitution })]);
+  // }, 8000)
 
 
-  // setTimeout(dropBalls, 12000);
+  var button = document.getElementById('drop-button')
+  console.log('butto dn', button)
+  button.addEventListener('click', function(ev){
+    console.log('the button was clicked', bottom);
+    Matter.Body.translate(bottom, {x:20, y:0})
+    // World.add(engine.world, [bottom])
+  });
+
 }
 
