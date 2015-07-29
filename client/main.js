47f416b46487bead1398861e0b8f360a382e7301
JayChetty <jaypaulchetty@gmail.com>
2015-07-29 09:50:30 +0100

    refactor

--- client/main.js
+++ client/main.js
@@ -54,56 +54,52 @@ window.onload = function(){
   var bottomWidth = 100
 
   var firstLineTopY = bottomY - boxHeightAdjust - pieceDiameter;
-
   var boxLeftX = centerX - boxWidth/2
   var boxRightX = centerX + boxWidth/2
   
-
+  var addGround = function(){
   // add ground
-  World.add(engine.world, [
-    Bodies.rectangle(centerX, bottomY, width, 1, { isStatic: true, angle: 0 }),
-  ]);
+    World.add(engine.world, [
+      Bodies.rectangle(centerX, bottomY, width, 1, { isStatic: true, angle: 0 }),
+    ]);
+  }
 
-  console.log('engine.world',engine.world)
-
+  var addCatchBox = function(){
   //add catch box
-  World.add(engine.world, [
-    //bottom
-    Bodies.rectangle(centerX, bottomY - boxHeightAdjust/2, boxWidth+pieceDiameter/2, 1, { isStatic: true, angle: boxAngle }),
-    //walls
-    Bodies.rectangle(boxLeftX, bottomY - boxHeight/2, boxHeight, 1, { isStatic: true, angle: Math.PI * 0.5 }),
-    Bodies.rectangle(boxRightX, bottomY - boxHeightAdjust - (boxHeight/2), boxHeight, 1, { isStatic: true, angle: Math.PI * 0.5 }),
-  ]);
+    World.add(engine.world, [
+      //bottom
+      Bodies.rectangle(centerX, bottomY - boxHeightAdjust/2, boxWidth+pieceDiameter/2, 1, { isStatic: true, angle: boxAngle }),
+      //walls
+      Bodies.rectangle(boxLeftX, bottomY - boxHeight/2, boxHeight, 1, { isStatic: true, angle: Math.PI * 0.5 }),
+      Bodies.rectangle(boxRightX, bottomY - boxHeightAdjust - (boxHeight/2), boxHeight, 1, { isStatic: true, angle: Math.PI * 0.5 }),
+    ]);
+  }
 
-
+  //feed variables
   var feedAngle = -Math.PI * 0.04
   var feedPointX = centerX + boxWidth/2;
   var feedPointY = bottomY - boxHeight - boxHeightAdjust - pieceDiameter
-
   var feedTotalWidth = width - feedPointX
   var feedLength = feedTotalWidth / Math.cos(feedAngle)
   var feedHeight = (feedTotalWidth * Math.tan(feedAngle)) * -1
   var feedCenterX = feedPointX + feedTotalWidth/2
   var blockSize = pieceDiameter*3
   var holeSize = pieceDiameter * 1.2
-
-  console.log('feedCenterX', feedCenterX)
-  console.log('feedLength', feedLength)
-  console.log('feedHeight', feedHeight)
-  console.log('feedTotalWidth', feedTotalWidth)
-
   var feedGuide = Bodies.rectangle(feedPointX - pieceDiameter, feedPointY, pieceDiameter * 5, 1, { isStatic: true, angle: Math.PI * 0.5 })
   // add feed
-  World.add(engine.world, [
-    //ramps
-    Bodies.rectangle(feedCenterX, feedPointY - feedHeight/2, feedLength, 1, { isStatic: true, angle: feedAngle }),
-    Bodies.rectangle(feedCenterX - holeSize/2, feedPointY - feedHeight - feedHeight/2 - holeSize*0.8, feedLength - holeSize, 1, { isStatic: true, angle: feedAngle*-1 }),
-    //block
-    Bodies.rectangle(feedPointX + feedTotalWidth, feedPointY - feedHeight - blockSize/2 , blockSize, 5, { isStatic: true, angle: Math.PI * 0.5 }),
-    //guide
-    feedGuide,
-    Bodies.rectangle(feedPointX, feedPointY + pieceDiameter, pieceDiameter * 2, 1, { isStatic: true, angle: -Math.PI * 0.5 })
-  ]);
+
+  var addFeed = function(){
+    World.add(engine.world, [
+      //ramps
+      Bodies.rectangle(feedCenterX, feedPointY - feedHeight/2, feedLength, 1, { isStatic: true, angle: feedAngle }),
+      Bodies.rectangle(feedCenterX - holeSize/2, feedPointY - feedHeight - feedHeight/2 - holeSize*0.8, feedLength - holeSize, 1, { isStatic: true, angle: feedAngle*-1 }),
+      //block
+      Bodies.rectangle(feedPointX + feedTotalWidth, feedPointY - feedHeight - blockSize/2 , blockSize, 5, { isStatic: true, angle: Math.PI * 0.5 }),
+      //guide
+      feedGuide,
+      Bodies.rectangle(feedPointX, feedPointY + pieceDiameter, pieceDiameter * 2, 1, { isStatic: true, angle: -Math.PI * 0.5 })
+    ]);
+  }
 
   var boxStartX = feedPointX
   var gap = pieceDiameter*1
@@ -111,22 +107,25 @@ window.onload = function(){
   var boxHeight = 10*pieceDiameter 
   var ballBoxBodies = [];
 
+
+  var addPieceBox = function(){
   //add box columns
-  for(var i=0; i<11; i++){// we need 11 lines to create 10 boxes
-    ballBoxBodies.push( Bodies.rectangle(boxStartX + (i*pieceDiameter), boxStartY - boxHeight/2, boxHeight, 1, { isStatic: true, angle: Math.PI * 0.5 }) )   
+    for(var i=0; i<11; i++){// we need 11 lines to create 10 boxes
+      ballBoxBodies.push( Bodies.rectangle(boxStartX + (i*pieceDiameter), boxStartY - boxHeight/2, boxHeight, 1, { isStatic: true, angle: Math.PI * 0.5 }) )   
+    }
+
+    World.add(engine.world, ballBoxBodies)
+
+    //add protector
+    World.add(engine.world, [
+      Bodies.rectangle(boxStartX, boxStartY + gap/2, gap, 1, { isStatic: true, angle: Math.PI * 0.5 }),
+    ]);
+
+    //add bottom  
+    var bottomWidth = 10*pieceDiameter 
+    var bottom = Bodies.rectangle(boxStartX + bottomWidth/2, boxStartY, bottomWidth, 1, { isStatic: true, angle: 0 });
+    World.add(engine.world, [bottom])
   }
-
-  World.add(engine.world, ballBoxBodies)
-
-  //add protector
-  World.add(engine.world, [
-    Bodies.rectangle(boxStartX, boxStartY + gap/2, gap, 1, { isStatic: true, angle: Math.PI * 0.5 }),
-  ]);
-
-  //add bottom  
-  var bottomWidth = 10*pieceDiameter 
-  var bottom = Bodies.rectangle(boxStartX + bottomWidth/2, boxStartY, bottomWidth, 1, { isStatic: true, angle: 0 });
-  World.add(engine.world, [bottom])
 
 
   // add pieces
@@ -146,24 +145,28 @@ window.onload = function(){
     8:"#995511",
     9:"#221111",
   }
-  for(var i=0; i<10; i++){
-    for(var j=0; j<timesTable; j++){
-      pieces.push( Bodies.circle(ballStartX + i*pieceDiameter, ballStartY - j*pieceDiameter, pieceDiameter/2, { friction: ballFriction, restitution: ballRestitution, 
-        frictionAir: ballFrictionAir, density: ballDensity, slop:ballSlop, render: { fillStyle: colours[i] } }) )
+
+  var addPieces = function(){
+    for(var i=0; i<10; i++){
+      for(var j=0; j<timesTable; j++){
+        pieces.push( Bodies.circle(ballStartX + i*pieceDiameter, ballStartY - j*pieceDiameter, pieceDiameter/2, { friction: ballFriction, restitution: ballRestitution, 
+          frictionAir: ballFrictionAir, density: ballDensity, slop:ballSlop, render: { fillStyle: colours[i] } }) )
+      }
     }
+
+    World.add(engine.world, pieces)
   }
 
-  World.add(engine.world, pieces)
+  addGround();
+  addCatchBox();
+  addFeed();
+  addPieceBox();
+  addPieces();
 
   Engine.run(engine);
 
-  var hasSupport1 = false
-  var hasSupport2 = false
-  var hasSupport3 = false
-  var hasSupport4 = false
+
   var button = document.getElementById('drop-button')
-
-
   var baseHeight = bottomY - (boxHeightAdjust/2)
   var channelHeight = pieceDiameter + 2
   var hasSupport = []
@@ -171,46 +174,39 @@ window.onload = function(){
     return(baseHeight - (num*channelHeight))
   }
 
-  console.log('butto dn', button)
-  button.addEventListener('click', function(ev){
-    Matter.Body.translate(bottom, {x:20, y:0})
+
+  var addShoot = function(){
     var count = 0;
     pieces.forEach(function(piece){
-      // console.log('piece', piece)
-      console.log('piece', piece)
-      // piece.setStatic(true)
       if(piece.position.y > feedPointY){
-        console.log('setting static')
         count++;
-        // Matter.Body.setStatic(piece, true);
       }
-      // if(count >=10){
-        // console.log('more than ten pieces adding helper')
-       
-
-      // }
-      // Matter.Sleeping(piece, true)
     })
 
     for(var i=0 ;i<10;i++){    
       if(!hasSupport[i] && count >= (i+1)*10){
-        console.log('adding a shoot')
         World.add(engine.world, [Bodies.rectangle(centerX, heightForGuide(i+1), boxWidth, 1, { isStatic: true, angle: boxAngle })])
         hasSupport[i] = true;
         if(i==7){
           Matter.Body.translate(feedGuide, {x:0, y: -1.5 * pieceDiameter})
         }
       }
-    }
+    }    
+  }
+
+  var drop = function(){
+    Matter.Body.translate(bottom, {x:20, y:0})
+    addShoot()
+  }
+
+  button.addEventListener('click', function(ev){
+    drop();
   });
 
-  console.log('engine', engine)
 
   var ctx = engine.render.context;
 
-
   var heightOffset = boxHeightAdjust/10
-
   var xGridSize = boxWidth/10
   Events.on(engine, 'afterRender',  function(ev){
     ctx.save();
